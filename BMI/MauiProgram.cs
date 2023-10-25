0aa38ed4672088b6173f117989517d9aa9afc9c3
Enrique <enriquearce00@gmail.com>
2023-10-25 00:30:15 -0600

    Dashboard | Syncfusion gauge (height & weight)

--- BMI/MauiProgram.cs
+++ BMI/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace BMI;
+﻿using Syncfusion.Maui.Core.Hosting;
+
+namespace BMI;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.ConfigureSyncfusionCore()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
---
+++ BMI/Views/DashboardView.xaml.cs
@@ -0,0 +1,9 @@
+namespace BMI.Views;
+
+public partial class DashboardView : ContentPage
+{
+	public DashboardView()
+	{
+		InitializeComponent();
+	}
+}
